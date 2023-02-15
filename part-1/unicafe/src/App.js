7027316f48fd3cc2f7b56c9984583ebc805a7a16
kapilRohilla <kapilrohilla2002@gmail.com>
2023-02-15 17:31:37 +0530

    Excercise 1.8, 1.9

--- part-1/unicafe/src/App.js
+++ part-1/unicafe/src/App.js
@@ -1,6 +1,29 @@
 import './App.css';
 import {useState} from "react";
 import Button from './component/button';
+
+const Stats = ({good,bad,neutral}) => {
+  let total = good + neutral + bad;
+  let average = (good-bad)/total;
+  let positive = good/total*100;
+  if(total===0){
+    return (
+        <p>No feedback given</p>
+    )
+  }else{
+    return (
+     <>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>All {total}</p>
+        <p>average {average}</p>
+        <p>positive {positive}%</p>
+     </> 
+    )
+  }
+}
+
 function App() {
   const [good,setGood] = useState(0);
   const [neutral,setNeutral] = useState(0);
@@ -10,10 +33,6 @@ function App() {
   const handleBad= () => setBad(bad+1);
   const handleNeutal= () => setNeutral(neutral+1);
   
-  let total = good + neutral + bad;
-  let average = (good-bad)/total;
-  let positive = good/total*100;
-  console.log(total,average,positive);
   return (
     <>
      <h1>Give Feedback</h1>
@@ -21,12 +40,8 @@ function App() {
      <Button handleClick={handleNeutal}text="neutral"/>
      <Button handleClick={handleBad} text="bad"/>
      <h2>Statistics</h2>
-     <p>good {good}</p>
-     <p>neutral {neutral}</p>
-     <p>bad {bad}</p>
-     <p>All {total}</p>
-     <p>average {average}</p>
-     <p>positive {positive}%</p>
+     <Stats good={good} bad={bad} neutral={neutral}/>
+    
     </>
   );
 }
