174402b15e857b7e9842ef38dadb9f7bb3d10fc7
kapilRohilla <kapilrohilla2002@gmail.com>
2023-02-21 23:03:02 +0530

    Excercise 2.6: The Phonebook Step1 completed

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,8 +1,47 @@
-function App() {
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  
+  const data = (e) => {
+    setNewName(e.target.value);
+  }
+  const insertData = () => {
+    if(newName.length > 0){
+      setPersons([
+        ...persons,
+        {name: newName}
+      ])
+    }
+    setNewName('');
+  }
+  const presentData = (e) => e.preventDefault();
+
   return (
-    <>
-    </>
-  );
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={presentData}>
+        <div>
+          name: <input value={newName} onChange={data}/>
+        </div>
+        <div>
+          <button type="submit" onClick={insertData}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div className="data">
+        {
+          persons.map(obj=>{
+          
+           return <p key={obj.name}>{obj.name}</p>
+         // use of name as key is suggested in quesion
+        }) }
+      </div>
+    </div>
+  )
 }
 
 export default App;
