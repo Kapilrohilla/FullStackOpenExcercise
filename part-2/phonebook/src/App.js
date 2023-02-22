68cc2ab3108b6ea0e785feab93da694de4fa7403
kapilRohilla <kapilrohilla2002@gmail.com>
2023-02-22 17:48:11 +0530

    Excercise 2.9: The Phonebook Step4

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,51 +1,33 @@
 import { useState } from 'react'
-/*
-  error in inserting data 
-  now inserting require 2 add Btn click while i want only one
-*/
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-123456', id: 1 }]) 
-
+  // states
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-123456', id: 1 }])
+  const [filter, setFilter] = useState("")
   const [newName, setNewName] = useState('');
-  const [newNumber,setNewNumber] = useState('')
-  const [dataToShow,setDataToShow] = useState( 
-    persons.map(data=> <p key={data.id}>{data.name}  {data.number}</p>) 
-  )
-  const nameData = (e) => setNewName(e.target.value);
-  const phoneData = (e) => setNewNumber(e.target.value);
-  const filterData = (e) => {
-    let regex = new RegExp(e.target.value,'gi');
-    setDataToShow(
-      persons.map((obj)=>{
-        if(regex.test(obj.name)){
-          return <p key={obj.id}>{obj.name} {obj.number}</p>
-        }
-        return null;
-      })
-    )
-  }
+  const [newNumber, setNewNumber] = useState('')
+
+  const handleNameData = (e) => setNewName(e.target.value);
+  const handlePhoneData = (e) => setNewNumber(e.target.value);
+  const handleFilter = (e) => setFilter(e.target.value)
+
   const insertData = () => {
     // names will an array that collect name's from our previous array 'person'
-    let names = persons.map(obj=>obj.name);
+    let names = persons.map(obj => obj.name);
     // alert condition
-    if(names.includes(newName)) alert(`${newName} already added to phonebook`)
-    else if(newName.length > 0){
+    if (names.includes(newName)) alert(`${newName} already added to phonebook`)
+    else if (newName.length > 0) {
       let newObj = {
         name: newName,
         number: newNumber,
-        id: persons[persons.length-1].id+1
+        id: persons[persons.length - 1].id + 1
       }
       setPersons([
         ...persons,
         newObj
       ]);
     }
-    setDataToShow(
-      persons.map(data=> <p key={data.id}>{data.name} {data.number}</p>)
-    )
     setNewName('');
     setNewNumber('');
-    console.log('data displayed');
   }
 
   const presentData = (e) => e.preventDefault();
@@ -54,23 +36,28 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <div>
-        filter shown with <input onChange={filterData}/>
+        filter shown with <input value={filter} onChange={handleFilter} />
       </div>
       <h2>add a new</h2>
       <form onSubmit={presentData}>
         <div>
-          name: <input value={newName} onChange={nameData}/> <br /><br />
+          name: <input value={newName} onChange={handleNameData} /> <br /><br />
         </div>
         <div>
-          number: <input value={newNumber} onChange={phoneData}/> 
+          number: <input value={newNumber} onChange={handlePhoneData} />
         </div>
         <div>
-          <button type="submit" onClick={()=>insertData()}>add</button>
+          <button type="submit" onClick={() => insertData()}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div className="data">
-        {dataToShow}
+        {persons.filter(person => {
+          const regex = new RegExp(filter, "i")
+          return regex.test(person.name)
+        }).map(person => {
+          return (<p key={person.id}>{person.name} {person.number}</p>)
+        })}
       </div>
     </div>
   )
