50d0a0008c4078172937213e3f0c602dbbfbc581
kapilRohilla <kapilrohilla2002@gmail.com>
2023-02-22 14:58:54 +0530

    pending Excercise 2.9 The Phonebook step 4

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,19 +1,29 @@
 import { useState } from 'react'
-
+/*
+  error in inserting data 
+  now inserting require 2 add Btn click while i want only one
+*/
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 }
-  ]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-123456', id: 1 }]) 
 
   const [newName, setNewName] = useState('');
   const [newNumber,setNewNumber] = useState('')
-  const nameData = (e) => {
-    setNewName(e.target.value);
+  const [dataToShow,setDataToShow] = useState( 
+    persons.map(data=> <p key={data.id}>{data.name}  {data.number}</p>) 
+  )
+  const nameData = (e) => setNewName(e.target.value);
+  const phoneData = (e) => setNewNumber(e.target.value);
+  const filterData = (e) => {
+    let regex = new RegExp(e.target.value,'gi');
+    setDataToShow(
+      persons.map((obj)=>{
+        if(regex.test(obj.name)){
+          return <p key={obj.id}>{obj.name} {obj.number}</p>
+        }
+        return null;
+      })
+    )
   }
-  const phoneData = (e) => {
-    setNewNumber(e.target.value);
-  }
-
   const insertData = () => {
     // names will an array that collect name's from our previous array 'person'
     let names = persons.map(obj=>obj.name);
@@ -25,13 +35,17 @@ const App = () => {
         number: newNumber,
         id: persons[persons.length-1].id+1
       }
-        setPersons([
-          ...persons,
-          newObj
-        ])
+      setPersons([
+        ...persons,
+        newObj
+      ]);
     }
+    setDataToShow(
+      persons.map(data=> <p key={data.id}>{data.name} {data.number}</p>)
+    )
     setNewName('');
     setNewNumber('');
+    console.log('data displayed');
   }
 
   const presentData = (e) => e.preventDefault();
@@ -39,6 +53,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={filterData}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={presentData}>
         <div>
           name: <input value={newName} onChange={nameData}/> <br /><br />
@@ -47,16 +65,12 @@ const App = () => {
           number: <input value={newNumber} onChange={phoneData}/> 
         </div>
         <div>
-          <button type="submit" onClick={insertData}>add</button>
+          <button type="submit" onClick={()=>insertData()}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div className="data">
-        {
-          persons.map((data)=>{
-             return <p key={data.id}>{data.name}  {data.number}</p> // use of name as key is suggested in quesion
-          }) 
-        }
+        {dataToShow}
       </div>
     </div>
   )
