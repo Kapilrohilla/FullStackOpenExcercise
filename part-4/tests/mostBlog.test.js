81ae991f552116f41cce757faa986defd2c64440
kapilrohilla <kapilrohilla2002@gmail.com>
2023-07-08 16:28:08 +0530

    excercise 4.6

---
+++ part-4/tests/mostBlog.test.js
@@ -0,0 +1,90 @@
+const { whoWriteMostBlog } = require("../utils/list_helper")
+
+
+describe('author with most number of blogs', () => {
+    const listWithMultipleBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'king',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 2,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'king',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 3,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'kapil',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 15,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'kapil',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 15,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'kapil',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 15,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 17,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 17,
+            __v: 0
+        }
+
+    ]
+    test('should be equal to "Edsger W. Dijkstra"', () => {
+        const result = whoWriteMostBlog(listWithMultipleBlog);
+        expect(result).toEqual([{
+            author: 'Edsger W. Dijkstra',
+            blogs: 3
+        }])
+    })
+    test('should return an Error', () => {
+        const result = whoWriteMostBlog([]);
+        expect(result).toMatch("blogs array is empty");
+    })
+    test('should return the same object', () => {
+        const result = {
+            author: "kapil",
+            likes: 10
+        }
+        expect(whoWriteMostBlog([result])).toEqual(result)
+    })
+});
--- part-4/utils/list_helper.js
+++ part-4/utils/list_helper.js
@@ -19,4 +19,36 @@ const favoriteBlog = (blogs) => {
     })
     return favBlog;
 }
-module.exports = { dummy, totalLikes, favoriteBlog };
+
+const whoWriteMostBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return "blogs array is empty"
+    }
+    if (blogs.length === 1) {
+        return blogs[0];
+    }
+    const authors = blogs.map(o => o.author).sort();
+    let noOfBlogByAuthor = [];
+    let mostBlog = 0;
+    authors.forEach((author, currentIndex, arr) => {
+
+
+        if (currentIndex === 0 || arr[currentIndex - 1] !== author) {
+            noOfBlogByAuthor = noOfBlogByAuthor.concat({
+                author: author,
+                blogs: 1
+            })
+        } else if (arr[currentIndex] === author) {
+            const indexOfMatchingBlog = noOfBlogByAuthor.findIndex(obj => obj.author === author);
+            noOfBlogByAuthor[indexOfMatchingBlog] = {
+                author: author,
+                blogs: noOfBlogByAuthor[indexOfMatchingBlog].blogs + 1
+            }
+            if (mostBlog < noOfBlogByAuthor[indexOfMatchingBlog].blogs) {
+                mostBlog = noOfBlogByAuthor[indexOfMatchingBlog].blogs
+            }
+        }
+    })
+    return [noOfBlogByAuthor.find(obj => obj.blogs === mostBlog)];
+}
+module.exports = { dummy, totalLikes, favoriteBlog, whoWriteMostBlog };
