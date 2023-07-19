1ae61dec019301a4bcb671384b70c5db1b3f716b
kapilrohilla <kapilrohilla2002@gmail.com>
2023-07-19 23:01:25 +0530

    add: delete , put entrypoint

--- part-4/controllers/blog.js
+++ part-4/controllers/blog.js
@@ -1,16 +1,18 @@
 const blogRouter = require('express').Router();
 const Blog = require('../model/mongo');
 
+// get blogs
 blogRouter.get('/', async (req, res) => {
     const blogs = await Blog.find({})
     res.status(200).json(blogs)
 })
+//get specific blog
 blogRouter.get('/:id', async (req, res) => {
     const id = req.params.id;
     const response = await Blog.findById(id)
     res.status(200).json(response).end();
 })
-
+// add new blog
 blogRouter.post('/', async (req, res) => {
     let body = req.body;
     if (!body.likes) {
@@ -21,5 +23,22 @@ blogRouter.post('/', async (req, res) => {
 
     res.status(201).json(response);
 })
+// delete blog
+blogRouter.delete('/:id', async (req, res) => {
+    let id = req.params.id;
+    await Blog.findByIdAndDelete(id);
+    res.status(200).json({
+        "success": `Blog with ${id} is deleted`
+    })
+})
+// update blog;
+blogRouter.put('/:id', async (req, res) => {
 
+    const id = req.params.id;
+    const updateBlog = req.body;
+    await Blog.findByIdAndUpdate(id, updateBlog);
+    res.status(200).json({
+        "success": `Blog with ${id} is successfull`
+    })
+})
 module.exports = blogRouter;
