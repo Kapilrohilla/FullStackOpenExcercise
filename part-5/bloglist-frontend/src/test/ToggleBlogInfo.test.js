5efe4a13129e6d22bc1cdca2481c6c6f46ddecfb
kapilrohilla <kapilrohilla2002@gmail.com>
2023-08-13 16:01:11 +0530

    Excercise - 5.13

--- part-5/bloglist-frontend/src/test/ToggleBlogInfo.test.js
+++ part-5/bloglist-frontend/src/test/ToggleBlogInfo.test.js
@@ -5,9 +5,7 @@ import ToggleBlogInfo from "../components/ToggleBlogInfo";
 import "@testing-library/jest-dom/extend-expect";
 
 describe("<ToggleBlogInfo />", () => {
-  let contaniers;
-
-  beforeAll(() => {
+  beforeEach(() => {
     const blogData = {
       title: "testing...",
       author: "tester",
@@ -18,7 +16,7 @@ describe("<ToggleBlogInfo />", () => {
     };
     const mock4HandleLikeBtn = jest.fn();
     const mock4HandleDeleteBtn = jest.fn();
-    contaniers = render(
+    render(
       <ToggleBlogInfo
         blog={blogData}
         handleLikeBtn={mock4HandleLikeBtn}
@@ -29,11 +27,33 @@ describe("<ToggleBlogInfo />", () => {
       </ToggleBlogInfo>
     );
   });
+
   test("render blog's title in ToggableBlogComponent", () => {
     const blogTitle = screen.getByText("testing...");
     expect(blogTitle).toBeDefined();
   });
-  test("rendered data shouldn't display url or number of likes", () => {
+
+  test("by default, rendered data shouldn't display url or number of likes", () => {
+    const urlElement = screen.queryByText("URL :- localhost:3000/tests");
+    expect(urlElement).toBeNull();
+    const likesElement = screen.queryByText("LIKES :-");
+    expect(likesElement).toBeNull();
+  });
+
+  test("after clicking the show btn, it should display url & number of likes", async () => {
+    const showBtn = screen.getByText("Show");
+    userEvent.click(showBtn);
+    const urlElement = screen.queryByText("URL :- localhost:3000/tests");
+    expect(urlElement).toBeDefined();
+    const likesElement = screen.queryByText("LIKES :-");
+    expect(likesElement).toBeDefined();
+  });
+
+  test("after clicking the hide Btn, it shouldn't display url & number of likes", () => {
+    const showBtn = screen.getByText("Show");
+    userEvent.click(showBtn);
+    const hideBtn = screen.getByText("Hide");
+    userEvent.click(hideBtn);
     const urlElement = screen.queryByText("URL :- localhost:3000/tests");
     expect(urlElement).toBeNull();
     const likesElement = screen.queryByText("LIKES :-");
