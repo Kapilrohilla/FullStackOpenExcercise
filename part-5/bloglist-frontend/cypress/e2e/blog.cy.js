18d01f3feabe29952e81a902068f3ed917cef848
kapilrohilla <kapilrohilla2002@gmail.com>
2023-08-15 15:45:45 +0530

    5.18 login successful & unsucessful test

--- part-5/bloglist-frontend/cypress/e2e/blog.cy.js
+++ part-5/bloglist-frontend/cypress/e2e/blog.cy.js
@@ -1,12 +1,6 @@
 describe("Blog app", () => {
   beforeEach(() => {
     cy.resetdb();
-    const user1 = {
-      name: "tester",
-      username: "tester",
-      password: "tester",
-    };
-    cy.adduser(user1);
     cy.visit("");
   });
   it("Login form is shown", function () {
@@ -15,5 +9,32 @@ describe("Blog app", () => {
     cy.get("@loginForm").contains("password");
     cy.get("@loginForm").find("input:first");
     cy.get("@loginForm").find("input:last");
+  });
+  describe("login", () => {
+    const user1 = {
+      name: "tester",
+      username: "tester",
+      password: "tester",
+    };
+    beforeEach(() => {
+      cy.adduser(user1);
+    });
+    it("successful when credentials are correct", function () {
+      cy.get("input:first").type(user1.username);
+      cy.get("input:last").type(user1.password);
+      cy.get("button").click();
+      cy.contains("login successful")
+        .should("have.css", "color", "rgb(0, 128, 0)")
+        .and("have.css", "border-style", "solid");
+      cy.contains(`${user1.username} logged in`);
+    });
+    it("fails with wrong credentials", function () {
+      cy.get("input:first").type(user1.username);
+      cy.get("input:last").type("wrong");
+      cy.get("button").click();
+      cy.contains("Wrong username or password")
+        .should("have.css", "color", "rgb(255, 0, 0)")
+        .and("have.css", "border-style", "solid");
+    });
   });
 });
