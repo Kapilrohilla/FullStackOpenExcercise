49756a9dea773345fce7be948696354686ad6975
kapilrohilla <kapilrohilla2002@gmail.com>
2023-08-15 14:15:07 +0530

    few required update

--- part-4/app.js
+++ part-4/app.js
@@ -1,43 +1,57 @@
-const config = require('./utils/config');
-const express = require('express');
-require('express-async-errors');
+const config = require("./utils/config");
+const express = require("express");
+require("express-async-errors");
 const app = express();
-const cors = require('cors');
-const blogRouter = require('./controllers/blog');
-const userRouter = require('./controllers/users');
-const loginRouter = require('./controllers/login');
-const logger = require('./utils/logger');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const { unknownEndpoint, errorHandler, requestLogger, tokenExtractor } = require('./utils/middleware');
+const cors = require("cors");
+const testRouter = require("./controllers/testing");
+const blogRouter = require("./controllers/blog");
+const userRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const {
+  unknownEndpoint,
+  errorHandler,
+  requestLogger,
+  tokenExtractor,
+} = require("./utils/middleware");
 
-morgan.token('postBody', function (req) {
-    return JSON.stringify(req.body)
-})
-app.use(morgan(':method :url :status : response-time ms - :res[content-length] :postBody'));
+morgan.token("postBody", function (req) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(
+    ":method :url :status : response-time ms - :res[content-length] :postBody"
+  )
+);
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
 logger.info(`connecting to ${config.MONGODB_URI}`);
 
-mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info('connected to Mongodb');
-    })
-    .catch((err) => {
-        logger.error('error connecting to mongoDB: ', err.message);
-    })
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to Mongodb");
+  })
+  .catch((err) => {
+    logger.error("error connecting to mongoDB: ", err.message);
+  });
 
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 app.use(tokenExtractor);
 
-app.use('/api/blogs', blogRouter);
-app.use('/api/user', userRouter);
-app.use('/api/login', loginRouter);
+if (process.env.NODE_ENV === "test") {
+  app.use("/api/testing", testRouter);
+}
+
+app.use("/api/blogs", blogRouter);
+app.use("/api/user", userRouter);
+app.use("/api/login", loginRouter);
 app.use(unknownEndpoint);
 app.use(errorHandler);
-
 
 module.exports = app;
